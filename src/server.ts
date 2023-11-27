6f19f15b4daf998db47c4d87cbfd6726b3a65d75
Shahzaib <shahzaibafzalofficial@gmail.com>
2023-11-27 20:12:24 +0500

    add typescript

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+function login(userName:string,password:string) : boolean{
+    return userName==="admin" && password==="secret"
+}
+
+if(login("admin","secret")){
+    console.log("Login Successfully");   
+}else{
+    console.log("Invalid credentials!"); 
+}
