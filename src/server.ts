b38a51af9b00c4cc3c650d4effa6db1e5b8881aa
Shahzaib <shahzaibafzalofficial@gmail.com>
2023-11-28 19:06:59 +0500

    husky test2

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
-function login(userName: string, password: string): boolean {
-  return userName === "admin" && password === "secret";
+function login(userName: string, password: string): boolean {return userName === "admin" && password === "secret";
 }
 
 if (login("admin", "secret")) {
-  console.log("Login Successfully");
+  // console.log("Login Successfully");
 } else {
-  console.log("Invalid credentials!");
+  // console.log("Invalid credentials!");
 }
