c6701d68e1d03b935460d25b7d51aa9bbd9089bf
Shahzaib <shahzaibafzalofficial@gmail.com>
2023-11-28 20:12:43 +0500

    express js setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
-// import { Config } from "./config";
+import app from "./app";
+import { Config } from "./config";
 
-// console.log(Config.NODE_ENV);
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is Listning on port ${PORT}`);
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
