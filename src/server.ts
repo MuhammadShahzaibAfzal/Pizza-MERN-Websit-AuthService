51881a21c699446b447746b74287bd287201d462
Shahzaib <shahzaibafzalofficial@gmail.com>
2023-11-28 18:16:02 +0500

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-function login(userName:string,password:string) : boolean{
-    return userName==="admin" && password==="secret"
+function login(userName: string, password: string): boolean {
+  return userName === "admin" && password === "secret";
 }
 
-if(login("admin","secret")){
-    console.log("Login Successfully");   
-}else{
-    console.log("Invalid credentials!"); 
+if (login("admin", "secret")) {
+  console.log("Login Successfully");
+} else {
+  console.log("Invalid credentials!");
 }
